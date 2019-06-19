513737fbac12b1e70ab9ca859da63e3a0c5bf953
JesseRM <jesserrm@gmail.com>
2019-06-18 19:30:09 -0700

    Create path for serving of static files from Express

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use('/api/images', require('./routes/api/images'));
+
+app.use(express.static('../client/build'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+})
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening for requests.');
