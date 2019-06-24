71a3fbe8ea27be61a5b551d5bc411ec3d85fa1fa
JesseRM <jesserrm@gmail.com>
2019-06-23 20:13:07 -0700

    Create useAsyncState function

---
+++ client/src/Util/asyncState.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export default function useAsyncState(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  const setter = x =>
+    new Promise(resolve => {
+      setValue(x);
+      resolve(x);
+    });
+  return [value, setter];
+}
