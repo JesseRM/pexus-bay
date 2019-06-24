b147fb6769594924af8de9ed904b6e74356c93a5
JesseRM <jesserrm@gmail.com>
2019-06-23 20:11:10 -0700

    Update getImgURIs helper function and include GetMoreButton

--- client/src/routes/Home/Home.js
+++ client/src/routes/Home/Home.js
@@ -2,16 +2,31 @@ import React, { useState } from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import axios from 'axios';
 import Pictures from '../../components/Pictures/Pictures';
+import GetMoreButton from '../../components/GetMoreButton/GetMoreButton';
+import useAsyncState from '../../Util/asyncState';
 
 export default function Home() {
   const [imgURIs, setImgURIs] = useState(null);
-  const [source, setSource] = useState('pexels');
-  const [page, setPage] = useState(1);
+  const [source, setSource] = useState('unsplash');
+  const [page, setPage] = useAsyncState(1);
   const [term, setTerm] = useState('');
 
-  function getImgURIs() {
+  function getImgURIs(page) {
     axios.get(`/api/images/${source}/${term}/${page}`).then((URIs) => {
-      setImgURIs(URIs.data);
+      setImgURIs((prevURIs) => {
+        if (page !== 1) {
+          const updatedURIs = {
+            thumb: [...prevURIs.thumb, ...URIs.data.thumb],
+            low: [...prevURIs.low, ...URIs.data.low],
+            medium: [...prevURIs.medium, ...URIs.data.medium],
+            high: [...prevURIs.high, ...URIs.data.high]
+          }
+
+          return updatedURIs;
+        }
+       
+        return URIs.data;
+      });
     })
   }
   
@@ -19,6 +34,7 @@ export default function Home() {
     <React.Fragment>
       <NavBar setTerm={setTerm} getImgURIs={getImgURIs} />
       <Pictures imgURIs={imgURIs} />
+      <GetMoreButton imgURIs={imgURIs} getImgURIs={getImgURIs} setPage={setPage} page={page} />
     </React.Fragment>
   )
 }
