f81ecef950ff5667443619fb937ed08c05747ef9
JesseRM <jesserrm@gmail.com>
2019-06-23 20:06:49 -0700

    Create GetMoreButton component

---
+++ client/src/components/GetMoreButton/GetMoreButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+export default function GetMoreButton(props) {
+  if (!props.imgURIs) {
+    return null;
+  }
+
+  function handleClick() {
+    console.log(props.page);
+    props.setPage(props.page + 1).then((page) => props.getImgURIs(page));
+  }
+
+  return (
+    <div>
+      <Button variant="contained" color="primary" onClick={handleClick}>
+        Get More
+      </Button>
+    </div>
+  )
+}
