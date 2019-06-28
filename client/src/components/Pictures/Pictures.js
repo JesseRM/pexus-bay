77d99a585f3e30dafaa16a9e045062495e01e917
JesseRM <jesserrm@gmail.com>
2019-06-27 21:49:59 -0700

    Update Pictures component to include icon button for each picture

--- client/src/components/Pictures/Pictures.js
+++ client/src/components/Pictures/Pictures.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import { makeStyles } from '@material-ui/core/styles';
-import PictureMenu from '../PictureMenu/PictureMenu'
-
+import PictureMenu from '../PictureMenu/PictureMenu';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -23,7 +24,12 @@ const useStyles = makeStyles(theme => ({
 
 export default function Pictures(props) {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
   let picCards;
+
+  function handleClick(event) {
+    setAnchorEl(event.currentTarget);
+  }
   
   if (props.imgURIs) {
     picCards = props.imgURIs.thumb.map((thumb, index) => {
@@ -31,7 +37,9 @@ export default function Pictures(props) {
       return (
         <Card className={classes.card} key={index}>
           <CardMedia className={classes.media} image={thumb}>
-            <PictureMenu className={classes.menuIcon}/>
+            <IconButton onClick={handleClick}>
+              <MoreVertIcon />
+            </IconButton>
           </CardMedia>
         </Card>
       )
@@ -41,6 +49,7 @@ export default function Pictures(props) {
   return (
     <div className={classes.root}>
       {picCards}
+      <PictureMenu anchorElement={anchorEl} setAnchorEl={setAnchorEl} />
     </div>
   )
 }
