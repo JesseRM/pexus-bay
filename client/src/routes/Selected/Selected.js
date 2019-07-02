ebe13b3f33deaf082246b2c5a26c6da381c3ad37
JesseRM <jesserrm@gmail.com>
2019-07-01 19:52:42 -0700

    Create Selected route

---
+++ client/src/routes/Selected/Selected.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Pictures from '../../components/Pictures/Pictures';
+
+export default function Selected() {
+
+  return (
+    <div>
+      <h1>Selected</h1>
+      <Pictures />
+    </div>
+  )
+}
