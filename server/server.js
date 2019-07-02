13e134ba4fb50d4a1a055551ba035be84820ed17
JesseRM <jesserrm@gmail.com>
2019-07-01 20:02:02 -0700

    Modify GET request

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ app.use('/api/images', require('./routes/api/images'));
 
 app.use(express.static('../client/build'));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-})
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening for requests.');
