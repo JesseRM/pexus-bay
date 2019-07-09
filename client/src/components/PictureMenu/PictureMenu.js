0d1dfe9eb19cb4662145acf9ee03aabd6f2e7fba
JesseRM <jesserrm@gmail.com>
2019-07-08 19:03:38 -0700

    Add icons to picture menu

--- client/src/components/PictureMenu/PictureMenu.js
+++ client/src/components/PictureMenu/PictureMenu.js
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import { makeStyles } from '@material-ui/core/styles';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import DownloadIcon from '@material-ui/icons/Archive';
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: '10px'
+  }
+}));
+
 
 export default function PictureMenu(props) {
   const [anchorEl, setAnchorEl] = useState(props.anchorElement);
+  const classes = useStyles();
 
   function handleClose() {
     props.setAnchorEl(null);
@@ -21,9 +32,11 @@ export default function PictureMenu(props) {
       keepMounted
     >
       <MenuItem>
+        <DownloadIcon className={classes.icon} />
         Download
       </MenuItem>
       <MenuItem>
+        <AddCircleIcon className={classes.icon} />
         Add to Selected
       </MenuItem>
     </Menu>
