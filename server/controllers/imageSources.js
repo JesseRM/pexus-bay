5c2f8ab6b5ed0cf70904d46a750e3d0969e0f3ec
JesseRM <jesserrm@gmail.com>
2019-07-12 21:37:13 -0700

    imgSources is now an array of objects

--- server/controllers/imageSources.js
+++ server/controllers/imageSources.js
@@ -3,24 +3,20 @@ const axios = require('axios');
 
 const getPixabay = (req, res) => {
   const url = `https://pixabay.com/images/search/${req.params.term}/?pagi=${req.params.page}`;
-  const imgSources = {
-    thumb: [],
-    low: [],
-    medium: [],
-    high: []
-  }
+  const imgSources = [];
   
   axios.get(url).then((html) => {
     const $ = cheerio.load(html.data);
 
     $('.item img[data-lazy]').each((i, elem) => {
-      const thumb = elem['attribs']['data-lazy'];
-      const low = thumb.replace(/_340/, '_480');
-      const medium = thumb.replace(/__340/, '_1280');
+      const imgSource = {
+        thumb: elem['attribs']['data-lazy'],
+        low: elem['attribs']['data-lazy'].replace(/_340/, '_480'),
+        medium: elem['attribs']['data-lazy'].replace(/__340/, '_1280'),
+        high: null
+      }
       
-      imgSources.thumb.push(thumb);
-      imgSources.low.push(low);
-      imgSources.medium.push(medium);
+      imgSources.push(imgSource);
     });
 
     res.send(imgSources);
@@ -29,26 +25,20 @@ const getPixabay = (req, res) => {
 
 const getPexels = (req, res) => {
   const url = `https://www.pexels.com/search/${req.params.term}/?page=${req.params.page}`;
-  const imgSources = {
-    thumb: [],
-    low: [],
-    medium: [],
-    high: []
-  }
+  const imgSources = [];
 
   axios.get(url).then((html) => {
     const $ = cheerio.load(html.data);
 
     $('.photo-item__img').each((i, elem) => {
-      const thumb = elem['attribs']['src'];
-      const low = thumb.replace('dpr=1', 'dpr=3');
-      const medium = thumb.replace('dpr=1&w=500', 'dpr=2&h=750&w=1260');
-      const high = thumb.split('?')[0];
+      const imgSource = {
+        thumb: elem['attribs']['src'],
+        low: elem['attribs']['src'].replace('dpr=1', 'dpr=3'),
+        medium: elem['attribs']['src'].replace('dpr=1&w=500', 'dpr=2&h=750&w=1260'),
+        high: elem['attribs']['src'].split('?')[0]
+      }
       
-      imgSources.thumb.push(thumb);
-      imgSources.low.push(low);
-      imgSources.medium.push(medium);
-      imgSources.high.push(high);
+      imgSources.push(imgSource);
     });
 
     res.send(imgSources);
@@ -57,21 +47,20 @@ const getPexels = (req, res) => {
 
 const getUnsplash = ((req, res) => {
   const url = `https://unsplash.com/napi/search/photos?query=${req.params.term}&xp=&per_page=30&page=${req.params.page}`;
-  const imgSources = {
-    thumb: [],
-    low: [],
-    medium: [],
-    high: []
-  }
+  const imgSources = [];
 
   axios.get(url).then((imagesJSON) => {
     const images = imagesJSON['data']['results'];
 
     for (let image in images) {
-      imgSources.thumb.push(images[image]['urls']['small']);
-      imgSources.low.push(images[image]['urls']['small']);
-      imgSources.medium.push(images[image]['urls']['regular']);
-      imgSources.high.push(images[image]['urls']['full']);
+      const imgSource = {
+        thumb: images[image]['urls']['small'],
+        low: images[image]['urls']['small'],
+        medium: images[image]['urls']['regular'],
+        high: images[image]['urls']['full']
+      }
+      
+      imgSources.push(imgSource);
     }
     
     res.send(imgSources);
