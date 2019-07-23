9cca7eef52193270824126b257d5283def268554
JesseRM <jesserrm@gmail.com>
2019-07-22 19:48:00 -0700

    Add download route to api

--- server/routes/api/images.js
+++ server/routes/api/images.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const imagesController = require('../../controllers/imageSources');
+const getImageBuffer = require('../../controllers/imageDownload');
 
 router.get('/pixabay/:term/:page', imagesController.getPixabay);
 router.get('/pexels/:term/:page', imagesController.getPexels);
 router.get('/unsplash/:term/:page', imagesController.getUnsplash);
+router.get('/download/*', getImageBuffer);
 
 module.exports = router;
