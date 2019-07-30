e20cc3490b2e74fbceeb331dc7ef406faec16d3c
JesseRM <jesserrm@gmail.com>
2019-07-29 19:10:12 -0700

    Create downloadImage function

--- client/src/components/DownloadMenu/DownloadMenu.js
+++ client/src/components/DownloadMenu/DownloadMenu.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import getImageBlob from '../../Util/getImageBlob';
+import createAnchorElFromBlob from '../../Util/createAnchorElFromBlob';
 
 export default function DownloadMenu(props) {
   
@@ -8,15 +10,27 @@ export default function DownloadMenu(props) {
     props.setDownloadMenuOpen(false);
   }
 
+  function downloadImage(imageURI) {
+    let anchorImageEl;
+
+    getImageBlob(imageURI).then((imageBlob) => {
+      anchorImageEl = createAnchorElFromBlob(imageBlob, 'image.jpg');
+
+      document.body.appendChild(anchorImageEl);
+      anchorImageEl.click();
+      document.body.removeChild(anchorImageEl);
+    });
+  }
+  
   return (
     <Menu 
     anchorEl={props.anchorEl}  
     open={props.downloadMenuOpen}
     onClose={handleClose}
     >
-      <MenuItem>Low</MenuItem>
-      <MenuItem>Medium</MenuItem>
-      <MenuItem>High</MenuItem>
+      <MenuItem onClick={() => downloadImage(props.clickedImg.low)}>Low</MenuItem>
+      <MenuItem onClick={() => downloadImage(props.clickedImg.medium)}>Medium</MenuItem>
+      <MenuItem onClick={() => downloadImage(props.clickedImg.high)}>High</MenuItem>
     </Menu>
   )
 }
