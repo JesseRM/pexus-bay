356760dcc4583ed034a22feeeb49f6282293ab67
JesseRM <jesserrm@gmail.com>
2019-07-29 19:04:12 -0700

    Get URI from req.url instead to get parameters

--- server/controllers/imageDownload.js
+++ server/controllers/imageDownload.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
 function getImageBuffer(req, res) {
-  const URI = req.params[0];
-  
+  const URI = req.url.slice(10);
+
   axios.get(URI, {responseType: 'arraybuffer'}).then((imageBuffer) => {
     res.send(imageBuffer.data);
   });
