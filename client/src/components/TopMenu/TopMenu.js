80416bb94fceea36dbe718590b434c6af8f27dbf
JesseRM <jesserrm@gmail.com>
2019-08-06 20:00:14 -0700

    Rename handleClose function, remove unnecessary variables and adjust formatting

--- client/src/components/TopMenu/TopMenu.js
+++ client/src/components/TopMenu/TopMenu.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -9,22 +9,35 @@ import HomeIcon from '@material-ui/icons/Home';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 
 export default function TopMenu(props) {
-  const [drawerState, setDrawerState] = useState(props.drawerState);
-
-  function handleClose() {
+  function handleTopMenuClose() {
     props.setDrawerState(false);
   }
 
   return (
-    <Drawer open={props.drawerState} onClose={handleClose}>
+    <Drawer 
+      open={props.drawerState} 
+      onClose={handleTopMenuClose}
+    >
       <List onClick={() => props.setDrawerState(false)}>
-      <ListItem button component={Link} to='/'>
-          <ListItemIcon><HomeIcon /></ListItemIcon>
-          <ListItemText primary='Home' />
+        <ListItem 
+          button 
+          component={Link} 
+          to='/'
+        >
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+            <ListItemText primary='Home' />
         </ListItem>
-        <ListItem button component={Link} to='/selected'>
-          <ListItemIcon><DoneIcon /></ListItemIcon>
-          <ListItemText primary='Selected' />
+        <ListItem 
+          button 
+          component={Link} 
+          to='/selected'
+        >
+          <ListItemIcon>
+            <DoneIcon />
+          </ListItemIcon>
+            <ListItemText primary='Selected' />
         </ListItem>
       </List>
     </Drawer>
