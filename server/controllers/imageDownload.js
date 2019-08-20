09720f7fcd79e7f7291604e6d73a007e93051208
JesseRM <jesserrm@gmail.com>
2019-08-19 20:37:01 -0700

    Handle possible error in axios GET request

--- server/controllers/imageDownload.js
+++ server/controllers/imageDownload.js
@@ -3,9 +3,13 @@ const axios = require('axios');
 function getImageBuffer(req, res) {
   const URI = req.url.slice(10);
 
-  axios.get(URI, {responseType: 'arraybuffer'}).then((imageBuffer) => {
-    res.send(imageBuffer.data);
-  });
+  axios.get(URI, {responseType: 'arraybuffer'})
+    .then((imageBuffer) => {
+      res.send(imageBuffer.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 } 
 
 module.exports = getImageBuffer;
