03c1a7bf9429abffabebadf50789bf81a4aac6b9
JesseRM <jesserrm@gmail.com>
2019-11-12 20:05:32 -0800

    Add attributes parameter

--- client/src/Util/createAnchorElFromBlob.js
+++ client/src/Util/createAnchorElFromBlob.js
@@ -1,8 +1,8 @@
-function createAnchorElFromBlob(imageBlob, imageName) {
+function createAnchorElFromBlob(fileBlob, attributes) {
   const anchorEl = document.createElement('a');
 
-  anchorEl.href = URL.createObjectURL(imageBlob);
-  anchorEl.download = imageName + '.jpg';
+  anchorEl.href = URL.createObjectURL(fileBlob);
+  anchorEl.download = attributes.filename + attributes.extension;
 
   return anchorEl;
 }
