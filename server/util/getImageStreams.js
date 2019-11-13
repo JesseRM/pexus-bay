3e4414b3d1ff9706f81af48d77e6e9bd9009282f
JesseRM <jesserrm@gmail.com>
2019-11-12 19:46:22 -0800

    Create getImageStreams utility function

---
+++ server/util/getImageStreams.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+async function getImageStreams(imageURIs) {
+  const promises = [];
+
+  for (let imageURI of imageURIs) {
+    const promise = new Promise((resolve, reject) => {
+      axios.get(imageURI.high, {responseType: 'stream'})
+        .then((imageStream) => {
+          resolve(imageStream);
+        })
+        .catch((error) => {
+          console.log(error);
+          reject(error);
+        });
+    });
+
+    promises.push(promise);
+  }
+
+  const imageStreams = await Promise.all(promises);
+  
+  return imageStreams;
+}
+
+module.exports = getImageStreams;
