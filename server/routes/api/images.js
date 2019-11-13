98e70293e6a2b338feb1f62dea3fbb68529208cc
JesseRM <jesserrm@gmail.com>
2019-11-12 20:00:21 -0800

    Create download/zip POST route

--- server/routes/api/images.js
+++ server/routes/api/images.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const imageBufferController = require('../../controllers/imageDownload');
+const imageDownloadController = require('../../controllers/imageDownload');
 const imageURIController = require('../../controllers/imageURIs');
 
 router.get('/pixabay/:term/:page', imageURIController.getPixabayURIs);
 router.get('/pexels/:term/:page', imageURIController.getPexelsURIs);
 router.get('/unsplash/:term/:page', imageURIController.getUnsplashURIs);
-router.get('/download/*', imageBufferController);
+router.get('/download/*', imageDownloadController.getImageBuffer);
+router.post('/download/zip', imageDownloadController.createZip);
 
 module.exports = router;
