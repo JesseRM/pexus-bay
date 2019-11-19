12a4b328dededcbc18f43a14c12e5f15528f0476
JesseRM <jesserrm@gmail.com>
2019-11-18 19:26:29 -0800

    Create CreateZipButton component

---
+++ client/src/components/CreateZipButton/CreateZipButton.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '80px'
+  }
+}));
+
+function CreateZipButton(props) {
+  const classes = useStyles();
+
+  if (!props.displayZipBtn) {
+
+    return null;
+  }
+
+  function handleClick() {
+    fetch('/api/images/download/zip', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify([...props.selectedImgs])
+    }).then((response) => {
+      response.blob().then((blob) => {
+        const anchorEl = document.createElement('a');
+
+        anchorEl.href = URL.createObjectURL(blob);
+        anchorEl.download = 'images.zip';
+        document.body.appendChild(anchorEl);
+        anchorEl.click();
+
+        URL.revokeObjectURL(anchorEl.href);
+        document.body.removeChild(anchorEl);
+      });
+    });
+  }
+
+  return (
+    <div className={classes.root}>
+      <Button 
+        variant="contained" 
+        color="secondary" 
+        onClick={handleClick}
+      >
+        Download Zip
+      </Button>
+    </div>
+  )
+}
+
+export default CreateZipButton;
