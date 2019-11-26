213972f093835d64229c1aeba8aaab49cff985db
JesseRM <jesserrm@gmail.com>
2019-11-25 20:26:06 -0800

    NavBar component now returns a root div component with AppBar and Progress components

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -7,6 +7,7 @@ import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
 import SourceMenu from '../SourceMenu/SourceMenu';
 import SearchBar from '../SearchBar/SearchBar';
+import Progress from '../Progress/Progress';
 
 const userStyles = makeStyles((theme) => ({
   titleHome: {
@@ -17,6 +18,13 @@ const userStyles = makeStyles((theme) => ({
   },
   title: {
     display: 'block'
+  },
+  root: {
+    position: 'fixed',
+    width: '100%',
+    top: 0,
+    left: 0,
+    zIndex: 1000
   }
 }));
 
@@ -24,34 +32,42 @@ function NavBar(props) {
   const classes = userStyles();
   
   return (
-    <AppBar position="fixed">
-      <Toolbar>
-        <IconButton onClick={() => props.setTopMenuOpen(true)}>
-          <MenuIcon />
-        </IconButton>
-        <Typography 
-          className={props.navBarType === 'home' ? classes.titleHome : classes.title} 
-          variant="h6" 
-          noWrap
-        >
-          Pexus Bay
-        </Typography>
-        {props.navBarType === 'home' &&
-          <SearchBar 
-            setTerm={props.setTerm} 
-            getImgURIs={props.getImgURIs} 
-            setGetMoreBtn={props.setGetMoreBtn} 
-          />
-        }
-        {props.navBarType === 'home' &&
-          <SourceMenu 
-            source={props.source}
-            setSource={props.setSource} 
-            setGetMoreBtn={props.setGetMoreBtn} 
-          />
-        }
-      </Toolbar>
-    </AppBar>
+    <div className={classes.root}>
+      <AppBar position='relative'>
+        <Toolbar>
+          <IconButton onClick={() => props.setTopMenuOpen(true)}>
+            <MenuIcon />
+          </IconButton>
+          <Typography 
+            className={props.navBarType === 'home' ? classes.titleHome : classes.title} 
+            variant="h6" 
+            noWrap
+          >
+            Pexus Bay
+          </Typography>
+          {props.navBarType === 'home' &&
+            <SearchBar
+              term={props.term}
+              source={props.source} 
+              setTerm={props.setTerm} 
+              getImgURIs={props.getImgURIs} 
+              setGetMoreBtn={props.setGetMoreBtn}
+              setImgURIs={props.setImgURIs} 
+              setDisplayGetMoreBtn={props.setDisplayGetMoreBtn}
+            />
+          }
+          {props.navBarType === 'home' &&
+            <SourceMenu 
+              source={props.source}
+              setSource={props.setSource} 
+              setGetMoreBtn={props.setGetMoreBtn}
+              setDisplayGetMoreBtn={props.setDisplayGetMoreBtn} 
+            />
+          }
+        </Toolbar>
+      </AppBar>
+      <Progress displayProgress={props.displayProgress} />
+    </div>
   )
 }
 
