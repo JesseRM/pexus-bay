0d93a886c28f0f307ca06f6696f3852a58d11a91
JesseRM <jesserrm@gmail.com>
2019-12-13 22:32:17 -0800

    Modify path for static files

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ require('dotenv').config();
 
 app.use(express.json());
 app.use('/api/images', require('./routes/api/images'));
-app.use(express.static('../client/build'));
+app.use('/', express.static('../client/build'));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
 });
 
