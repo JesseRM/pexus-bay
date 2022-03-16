e3ba1c1e62ce545dd2956e65910c5280b1f763f0
JesseRM <jesserrm@gmail.com>
2022-03-16 01:05:14 -0700

    Workaround for heroku deployment issues

--- client/src/Util/getImageBlob.js
+++
@@ -1,7 +0,0 @@
-function getImageBlob(imageURI) {
-  return fetch(`/api/images/download/${imageURI}`).then((imageBuffer) => {
-    return imageBuffer.blob();
-  });
-}
-
-export default getImageBlob;
--- client/src/Util/startFileDownload.js
+++
@@ -1,9 +0,0 @@
-function startFileDownload(anchorEl) {
-  document.body.appendChild(anchorEl);
-  anchorEl.click();
-
-  URL.revokeObjectURL(anchorEl.href);
-  document.body.removeChild(anchorEl);
-}
-
-export default startFileDownload;
