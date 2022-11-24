f1693c313392ada2f04932d26b17c34b7df16c1e
JesseRM <jesserrm@gmail.com>
2022-11-23 23:06:30 -0800

    Remove path import

--- server/server.js
+++ server/server.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
 require('dotenv').config();
 
 app.use(express.json());
 app.use('/api/images', require('./routes/api/images'));
-app.use('/static', express.static(path.resolve(__dirname, '../client', 'build', 'static')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening for requests.');
