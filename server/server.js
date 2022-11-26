f5fb811351007ea42b44068bfaf47b23c8e7301b
JesseRM <jesserrm@gmail.com>
2022-11-26 15:51:11 -0800

    Add CORS options to allow client to connect

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 require('dotenv').config();
 
+const corsOptions = {
+  origin: ["https://pexusbay.onrender.com"],
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/images', require('./routes/api/images'));
 
