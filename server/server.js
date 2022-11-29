e00ee6b5b78f4a06a08b0a15de3daa52b89ebfc4
JesseRM <jesserrm@gmail.com>
2022-11-28 22:08:11 -0800

    Add localhost to allowed origins

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const corsOptions = {
-  origin: ["https://pexusbay.onrender.com"],
+  origin: ["https://pexusbay.onrender.com", "http://localhost:3000"],
   optionsSuccessStatus: 200
 };
 
@@ -13,6 +13,6 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/images', require('./routes/api/images'));
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log('Listening for requests.');
 });
